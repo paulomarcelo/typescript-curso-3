13aa8d2037e9aec3b7419c648ef88134d7130033
paulomarcelo <paulo.marcelo@gmail.com>
2023-12-10 14:15:42 -0300

    Implementando a função paraTexto

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,9 +1,17 @@
 export class Negociacao {
     constructor(
-        private _data: Date, 
-        public readonly quantidade: number, 
+        private _data: Date,
+        public readonly quantidade: number,
         public readonly valor: number
-    ) {}
+    ) { }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
 
     get volume(): number {
         return this.quantidade * this.valor;
@@ -14,11 +22,11 @@ export class Negociacao {
         return data;
     }
 
-    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
-        const exp = /-/g;
-        const date = new Date(dataString.replace(exp, ','));
-        const quantidade = parseInt(quantidadeString);
-        const valor = parseFloat(valorString);
-        return new Negociacao(date, quantidade, valor);
+    public paraTexto(): string {
+        return `
+            Data: ${this.data},
+            Quantidade: ${this.quantidade},
+            Valor: ${this.valor}
+        `;
     }
 }
