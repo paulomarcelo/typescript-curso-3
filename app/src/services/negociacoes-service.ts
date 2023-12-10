12832baca0efed4ffd77c557f3da692591ef333f
paulomarcelo <paulo.marcelo@gmail.com>
2023-12-10 13:59:30 -0300

    Camada de serviços

---
+++ app/src/services/negociacoes-service.ts
@@ -0,0 +1,19 @@
+import { NegocicacaoDoDia } from "../interfaces/negociacao-do-dia.js";
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacoesService {
+
+    public obterNegociacoesDoDia(): Promise<Negociacao[]> {
+        return fetch('http://localhost:8080/dados')
+            .then(res => res.json())
+            .then((dados: NegocicacaoDoDia[]) => {
+                return dados.map(dadoDeHoje => {
+                    return new Negociacao(
+                        new Date(),
+                        dadoDeHoje.vezes,
+                        dadoDeHoje.montante
+                    )
+                })
+            });
+    }
+}
